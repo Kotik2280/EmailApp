76af687125ae1131eaa4e1f8cdf3a3e84a3e9536
Артём Назаров <brokeprotocol4916@gmail.com>
2025-04-25 18:22:12 +0500

    Adding form validation system

--- EmailApp/Models/MessageData.cs
+++ EmailApp/Models/MessageData.cs
@@ -1,11 +1,16 @@
-﻿namespace EmailApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailApp.Models
 {
     public class MessageData
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Не указано сообщение")]
+        [StringLength(200, ErrorMessage = "Слишком длинное сообщение")]
         public string Message { get; set; }
         public string SenderEmail { get; set; }
+        [Required(ErrorMessage = "Не указан получатель сообщения")]
         public string ReceiverEmail { get; set; }
     }
 }
--- EmailApp/Models/User.cs
+++ EmailApp/Models/User.cs
@@ -1,9 +1,16 @@
-﻿namespace EmailApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailApp.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Не указан Email")]
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
+        [StringLength(32, MinimumLength = 6, ErrorMessage = "Длина Email должна быть от 6 до 50 символов")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Не указан пароль")]
+        [StringLength(32, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 32 символов")]
         public string Password { get; set; }
     }
 }
